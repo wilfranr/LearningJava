9142cb72bfd34749e8572eb384d831456c6f4b2e
wilfranr <wlfranr@gmail.com>
2021-08-02 18:51:54 -0500

    Se agregan botones y se les da funcionalidad

--- src/views/Interfaz.java
+++ src/views/Interfaz.java
@@ -1,11 +1,15 @@
 package views;
 
 import javax.swing.JFrame;
+
+import controller.Controlador;
+
 import java.awt.BorderLayout;
 
 public class Interfaz extends JFrame{
 
     private PanelOpciones panelOpciones;
+    private Controlador controlador;
 
     public Interfaz () {
         super();
@@ -14,16 +18,25 @@ public class Interfaz extends JFrame{
         setTitle("Wallet");
         setLayout(new BorderLayout());
 
+        controlador = new Controlador();
+
         PanelOpciones panel = new PanelOpciones(this);
 
         add(panel, BorderLayout.SOUTH);
 
 
     }
+    public void addClient(String nombre) {
+        controlador.addClient(nombre);
+    }
+    public String listarClientes() {
+        return controlador.listarClientes();
+    }
     public static void main(String[] args) {
         Interfaz vistaPrincipal = new Interfaz();
         vistaPrincipal.setVisible(true);
     }
+    
     
 }
 
--- src/views/PanelOpciones.java
+++ src/views/PanelOpciones.java
@@ -13,15 +13,19 @@ public class PanelOpciones extends JPanel implements ActionListener {
 
     public final static String BTN_SALDO = "Consultar saldo";
     public final static String BTN_CARGAR = "Cargar saldo";
+    public final static String BTN_CREAR = "Crear Clientes";
+    public final static String BTN_LISTAR = "Listar Clientes";
 
     //Se crean realciones o atributos
     private JButton btnSaldo;
     private JButton btnCargar;
+    private JButton btnCrear;
+    private JButton btnListar;
     private Interfaz interfaz;
     //Contructor
     public PanelOpciones(Interfaz inter) {
         super();
-        setLayout(new GridLayout(1,2));
+        setLayout(new GridLayout(1,4));
         setBorder(new TitledBorder("Opciones"));
 
         interfaz = inter;
@@ -29,17 +33,36 @@ public class PanelOpciones extends JPanel implements ActionListener {
         //Inicializar las relaciones o atributos
         btnSaldo = new JButton(BTN_SALDO);
         btnCargar = new JButton(BTN_CARGAR);
+        btnCrear = new JButton(BTN_CREAR);
+        btnListar = new JButton(BTN_LISTAR);
 
         //Modificar las propiedades
         btnSaldo.setActionCommand(BTN_SALDO);
         btnCargar.setActionCommand(BTN_CARGAR);
+        btnCrear.setActionCommand(BTN_CREAR);
+        btnListar.setActionCommand(BTN_LISTAR);
 
         btnSaldo.addActionListener(this);
         btnCargar.addActionListener(this);
+        btnCrear.addActionListener(this);
+        btnListar.addActionListener(this);
 
         //Agregar las relaciones o atributos
         add(btnSaldo);
         add(btnCargar);
+        add(btnCrear);
+        add(btnListar);
+    }
+    public void addClient() {
+        String nombre = JOptionPane.showInputDialog(interfaz, "Digita el nombre del cliente: ");
+        if (nombre!=null && !nombre.equals("")) {
+            interfaz.addClient(nombre);
+        }else if (nombre.equals("")) {
+            JOptionPane.showMessageDialog(interfaz, "Revisa los datos");
+        }
+    }
+    public void listarClientes() {
+        JOptionPane.showMessageDialog(interfaz, interfaz.listarClientes());
     }
 
     @Override
@@ -56,6 +79,12 @@ public class PanelOpciones extends JPanel implements ActionListener {
             System.out.println(wallet1.saveMoney(200000));
             wallet1.saveMoney(Integer.parseInt(JOptionPane.showInputDialog(interfaz, "Ingrese la cantidad a depositar: ")));
             JOptionPane.showMessageDialog(interfaz, "su nuevo saldo es: $"+wallet1.getSaldo());
+        }
+        else if (e.getActionCommand().equals(BTN_CREAR)) {
+            addClient();
+        }
+        else if (e.getActionCommand().equals(BTN_LISTAR)) {
+            listarClientes();
         }
         
     }
